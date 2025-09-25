761f8353c11123ce6a1b176668ca1a224d3a4324
JINHYUN-BRIGHTEVERYDAY <jinibrighteveryday@gmail.com>
2025-09-25 11:35:06 +0900

    feat: 메서드, 오버로딩 연습문제

---
+++ src/main/java/com/korit/study/ch06/메서드연습문제4.java
@@ -0,0 +1,17 @@
+package com.korit.study.ch06;
+/*
+    매개변수 있음, 리턴값 있음
+    정수를 받아서 제곱값을 반환하는 함수를 작성하세요
+    public static int ______(int number) {
+    여기에 코드 작성
+    }
+*/
+public class 메서드연습문제4 {
+    public static void main(String[] args) {
+        System.out.println(doubleNumber(2));
+    }
+
+    public static int doubleNumber (int number) {
+        return number * number;
+    }
+}
