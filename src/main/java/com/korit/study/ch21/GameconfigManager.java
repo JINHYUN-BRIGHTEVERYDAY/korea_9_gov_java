fee103ed8ed70acb9782e3781b5ee7cb1b892c2e
JINHYUN-BRIGHTEVERYDAY <jinibrighteveryday@gmail.com>
2025-10-17 09:18:39 +0900

    Feat: ããGameconfigManager 문제풀이

--- src/main/java/com/korit/study/ch21/GameconfigManager.java
+++ src/main/java/com/korit/study/ch21/GameconfigManager.java
@@ -5,19 +5,29 @@ import java.util.Objects;
 public class GameConfigManager {
     // TODO: 싱글톤 구현을 위한 필드들
 
+    private static GameConfigManager instance;
+
     // 설정 정보 필드들
     private int soundVolume = 50;           // 기본값 50
     private String resolution = "1280x720"; // 기본 해상도
 
     // TODO: 생성자 구현
 
-    public GameConfigManager(int soundVolume, String resolution) {
+    private GameConfigManager(int soundVolume, String resolution) {
         this.soundVolume = soundVolume;
         this.resolution = resolution;
     }
 
 
     // TODO: 싱글톤 인스턴스 반환 메서드 구현
+
+
+    public static GameConfigManager getInstance() {
+        if (Objects.isNull(instance)) {
+            instance = new GameConfigManager(50, "1280 * 720");
+        }
+        return instance;
+    }
 
     // Getter/Setter 메서드들
     public void setSoundVolume(int volume) {
@@ -43,7 +53,7 @@ public class GameConfigManager {
 
 
     public String getConfigInfo() {
-        return String.format("Sound: %d, Resolution: %s,
+        return String.format("Sound: %d, Resolution: %s",
                 soundVolume, resolution);
     }
 }
